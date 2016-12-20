c602cd1fc8431ee65d02bd479b9c26e45c3f394a
Eduard Bardají Puig <ebardaji@gmail.com>
2016-12-20 16:08:39 +0100

    creates component and app to display it

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import myComponent from './component'
+
+ReactDOM.render(
+  React.createElement(myComponent),
+  document.getElementById('app')
+)
